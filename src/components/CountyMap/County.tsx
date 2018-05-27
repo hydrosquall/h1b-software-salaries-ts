d4bebe2e5801ef56de8266e9869d63af54ed9833
Cameron Yick <cameron.yick@yale.edu>
2018-05-27 11:41:03 -0400

    Set Interface for County Component

--- src/components/CountyMap/County.tsx
+++ src/components/CountyMap/County.tsx
@@ -1,36 +1,58 @@
+import { GeoPermissibleObjects } from "d3";
+import { GeometryObject } from "geojson";
 import _ from "lodash";
 import React, { Component } from "react";
+
 
 const BlankColor = "rgb(240,240,240)";
 
 // REPLACE WITH D3-SCALE
 const ChoroplethColors = _.reverse([
-  'rgb(247,251,255)',
-  'rgb(222,235,247)',
-  'rgb(198,219,239)',
-  'rgb(158,202,225)',
-  'rgb(107,174,214)',
-  'rgb(66,146,198)',
-  'rgb(33,113,181)',
-  'rgb(8,81,156)',
-  'rgb(8,48,107)'
+  "rgb(247,251,255)",
+  "rgb(222,235,247)",
+  "rgb(198,219,239)",
+  "rgb(158,202,225)",
+  "rgb(107,174,214)",
+  "rgb(66,146,198)",
+  "rgb(33,113,181)",
+  "rgb(8,81,156)",
+  "rgb(8,48,107)"
 ]);
 
-class County extends Component<any, any> {
+interface IProps {
+  readonly zoom: object | null;
+  readonly value: number;
+  readonly key: string | number | undefined;
+  readonly geoPath: d3.GeoPath<any, GeoPermissibleObjects>;
+  readonly quantize: d3.ScaleQuantize<number>;
+  readonly feature: GeoJSON.Feature<GeometryObject, {}>;
+}
+
+class County extends Component<IProps, object> {
+  public shouldComponentUpdate(nextProps: IProps, nextState: object) {
+    const { zoom, value } = this.props;
+    const { zoom: nextZoom, value: nextValue } = nextProps;
+    return (zoom !== nextZoom) || (value !== nextValue);
+  }
+
   public render() {
     const { value, geoPath, feature, quantize } = this.props;
 
     let color = BlankColor;
-
     if (value) {
       color = ChoroplethColors[quantize(value)];
     }
-
-    return (<path
-              d={geoPath(feature)}
-              style={{fill: color}}
-              // title={feature.id} // not supported 
-            />)
+  
+    const featurePath = geoPath(feature);
+    if (!featurePath) {
+      return null
+    }  
+    return (
+      <path
+        d={featurePath}
+        style={{ fill: color }}
+      />
+    );
   }
 }
 
