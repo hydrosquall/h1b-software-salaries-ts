c9d95ca639837400a2f0d4955761182f925b3cb9
Cameron Yick <cameron.yick@yale.edu>
2018-06-02 14:46:35 -0400

    Fix Jest Config

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
 import { configure } from "enzyme";
-import * as Adapter from "enzyme-adapter-react-16";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
 import raf from "./rafPolyfill"; 
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new ReactSixteenAdapter() });
